529a21e6eb1d2b9c1b56ced037bcecc637253123
mihalin <stopa3.10.1996@mail.ru>
2020-12-17 11:31:07 +0300

    Python package template

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -1,2 +1,4 @@
 def say_hello():
-    print("Hello")
+    hello = "hello"
+    print(hello)
+    return hello
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,5 +3,5 @@ from hello_world import say_hello
 
 
 class TestHello(TestCase):
-    def test_hello_none(self):
-        self.assertTrue(say_hello() is None)
+    def test_hello_say(self):
+        self.assertTrue(say_hello() == "hello")
