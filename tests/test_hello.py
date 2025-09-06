25c1d3fa9635804bc281a8a1e26611a353861f21
Sergey Zapunidi <zapunidy@gmail.com>
2025-09-06 15:53:47 +0300

    Fixed PEP8 and excluded .tox from linter dirs

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,7 +5,7 @@ from hello_world import say_hello, get_version
 class TestHello(TestCase):
     def test_hello_say(self):
         self.assertTrue(say_hello() == "hello")
-    
+
     def test_get_version(self):
         version = get_version()
         self.assertIsInstance(version, str)
